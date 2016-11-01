3bb0a56b794c00681f17ed830dda894d05f2ab0b
Ian Schoonover <iantskon@gmail.com>
2016-11-01 12:05:54 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var name = prompt("What's your name?");
+
+alert('your name is ' + name + '!');
+
+console.log("Your name is " + name + '!');
+
+// ctrl + shift + j to open the console in chrome (cmd + option + j - MAC)
